4d0a81fbb16908f89dab2eb6c06dd9ac85280e9c
Stanislav Termosa <termosa.stanislav@gmail.com>
2022-01-21 09:11:00 +0200

    Updates

---
+++ precache-manifest.23a3e273c26ccb010788cecf395f3c10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d621e559140e7af6bc378c27ba2430a",
+    "url": "/use-request/index.html"
+  },
+  {
+    "revision": "d2a45b41c47bf93f4715",
+    "url": "/use-request/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "d7c82280ffcef8ed4e26",
+    "url": "/use-request/static/js/2.dcc13955.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/use-request/static/js/2.dcc13955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a45b41c47bf93f4715",
+    "url": "/use-request/static/js/main.0f1048f8.chunk.js"
+  },
+  {
+    "revision": "34544357b826314d2e10",
+    "url": "/use-request/static/js/runtime-main.f57d8297.js"
+  }
+]);
